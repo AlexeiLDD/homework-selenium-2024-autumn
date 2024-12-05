a79aa55a6de4efffd5cbeb1df9a633f18ffc5cc3
MaratKamalovPD <dead_island_hd@bk.ru>
2024-12-05 18:01:26 +0300

    audiences and leadforms UPD

--- hw/code/ui/locators/audience_locators.py
+++ hw/code/ui/locators/audience_locators.py
@@ -1,43 +1,44 @@
 from selenium.webdriver.common.by import By
 
 class AudiencesLocators:
-    NO_AUDIENCES_LABEL = (By.XPATH, "//*[@id='audience']/div/div[2]/div/div[2]/div/div/h2")
+    AUDIENCES_BUTTON = (By.XPATH, "//a[@data-testid='left-menu' and contains(., 'Аудитории')]")
+    NO_AUDIENCES_LABEL = (By.XPATH, "//h2[contains(@class, 'vkuiPlaceholder__header') and text()='Аудиторий пока нет']")
 
-    LIST_USERS_TAB = (By.XPATH, "//*[@id='tab_audience.users_list']")
-    NO_LIST_USERS_LABEL = (By.XPATH, "//*[@id='audience.users_list']/div/div[2]/div/div[2]/div/div/h2")
+    LIST_USERS_TAB = (By.XPATH, "//div[@data-testid='tabs-item' and @id='tab_audience.users_list']")
+    NO_LIST_USERS_LABEL = (By.XPATH, "//h2[text()='Списков пользователей пока нет']")
 
-    OFFLINE_CONVERSIONS_TAB = (By.XPATH, "//*[@id='tab_audience.offline_conversion']")
-    NO_OFFLINE_CONVERSIONS_LABEL = (By.XPATH, "// *[ @ id = 'audience.offline_conversion'] / div / div[2] / div / div[2] / div / div / h2")
+    OFFLINE_CONVERSIONS_TAB = (By.XPATH, "//div[@data-testid='tabs-item' and @id='tab_audience.offline_conversion']")
+    NO_OFFLINE_CONVERSIONS_LABEL = (By.XPATH, "//h2[text()='Списков офлайн-конверсий пока нет']")
 
-    CREATE_AUDIENCE_BUTTON = (By.XPATH, "// *[ @ id = 'audience'] / div / div[2] / div / div[2] / div / div / div[2] / div / button")
-    CREATE_AUDIENCE_HEADER_LABEL = (By.XPATH, "// *[ @ id = 'root'] / div / div[2] / div / div[2] / form / div[1] / h2")
+    CREATE_AUDIENCE_BUTTON = (By.XPATH, "//button[span/span[text()='Создать аудиторию']]")
+    CREATE_AUDIENCE_HEADER_LABEL = (By.XPATH, "//h2[text()='Создание аудитории']")
 
-    ADD_SOURCE_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div/div[1]/div/section[2]/div/button")
-    INCLUDE_SOURCE_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[1]/h2")
+    ADD_SOURCE_BUTTON = (By.XPATH, "//button[span/span[text()='Добавить источник']]")
+    INCLUDE_SOURCE_HEADER_LABEL = (By.XPATH, "//h2[text()='Включить источник']")
 
-    KEY_WORDS_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/section/div[3]")
-    KEY_WORDS_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[1]/h2")
+    KEY_WORDS_BUTTON = (By.XPATH, "//div[contains(@class, 'SourceType_button__c4PpZ')]//span[text()='Ключевые фразы']")
+    KEY_WORDS_HEADER_LABEL = (By.XPATH, "//h2[text()='Ключевые фразы']")
 
-    KEY_WORDS_INPUT = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/section/div[2]/div[1]/span[1]/textarea")
-    KEY_WORDS_HINT_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[2]/div/div[1]/section/div[2]/div[1]/span[2]/div/div/div")
-    KEY_WORDS_HINT_WINDOW = (By.XPATH, "/html/body/div[2]/div/div[2]")
+    KEY_WORDS_INPUT = (By.XPATH, "//textarea[@placeholder='Введите фразу и нажмите Enter']")
+    KEY_WORDS_HINT_LABEL = (By.XPATH, "//div[contains(@class, 'Hint_hintTrigger__ixYRu') and starts-with(text(), 'Показать')]")
+    KEY_WORDS_HINT_WINDOW = (By.XPATH, "//button[contains(@class, 'vkuiButton--mode-tertiary') and span/span[text()='Отмена']]")
 
-    KEY_WORDS_HINT_ADD_BUTTON = (By.XPATH, "/html/body/div[2]/div/div[2]/div[2]/button[2]")
+    KEY_WORDS_HINT_ADD_BUTTON = (By.XPATH, "//button[contains(@class, 'vkuiButton--mode-primary') and span/span[text()='Добавить все']]")
 
-    KEY_WORDS_SAVE_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[2]/div/div[2]/div/button[1]")
-    KEY_WORDS_ITEM_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div/div[1]/div/section[2]/div/div/div/div/div/div/h4")
+    KEY_WORDS_SAVE_BUTTON = (By.XPATH, "//button[contains(@class, 'vkuiButton--mode-primary') and span/span[text()='Добавить все']]")
+    KEY_WORDS_ITEM_HEADER_LABEL = (By.XPATH, "//h4[contains(@class, 'vkuiHeadline--level-1') and text()='Ключевые фразы']")
 
-    SAVE_AUDIENCE_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div/div[2]/div/button[1]")
+    SAVE_AUDIENCE_BUTTON = (By.XPATH, "//button[@data-testid='submit' and span/span[text()='Сохранить']]")
     AUDIENCE_ITEM = (By.XPATH, "//*[@id='audience']/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[1]/div/div/div[2]/div/h5")
     AUDIENCE_CHECKBOX = (By.XPATH, "//*[@id='audience']/div/div[2]/div/div[2]/div/div/div/div/div[2]/div[2]/div/div/div[1]/label")
 
-    SHARE_AUDIENCE_BUTTON = (By.XPATH, "//*[@id='audience']/div/div[2]/div/div[1]/div[1]/button[1]")
+    SHARE_AUDIENCE_BUTTON = (By.XPATH, "//span[contains(@class, 'vkuiButton__in')]/span/svg[use[@xlink:href='#share_outline_24']]")
     OPEN_ACCESS_HEADER_LABEL = (By.XPATH, "//*[@id='_modal_40-label']")
 
     SAVE_SHARE_AUDIENCE_BUTTON = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/div/div[2]/div/button[2]")
     CANCEL_SHARE_AUDIENCE_BUTTON = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/div/div[2]/div/button[1]")
 
-    DELETE_AUDIENCE_BUTTON = (By.XPATH, "//*[@id='audience']/div/div[2]/div/div[1]/div[1]/button[2]")
+    DELETE_AUDIENCE_BUTTON = (By.XPATH, "//span[contains(@class, 'vkuiButton__in')]/span/svg[use[@xlink:href='#delete_outline_24']]")
     DELETE_AUDIENCE_HEADER_LABEL = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/div/div/div/h2")
 
     DELETE_AUDIENCE_INSIDE_MODAL_DELETE_BUTTON = (By.XPATH, "/html/body/div[1]/div/div[2]/div/div[2]/div/div/div/div[2]/div[1]/div/div/div/div/button[2]")
--- hw/code/ui/locators/leadforms_locators.py
+++ hw/code/ui/locators/leadforms_locators.py
@@ -1,51 +1,51 @@
 from selenium.webdriver.common.by import By
 
 class LeadFormsLocators:
-    NO_LEADFORMS_LABEL = (By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div[1]/div/div[2]/div/main/div[2]/div/div/div[2]/section/div/div/div/div/div/div[2]/div[2]/div/div/div/div/h2")
+    LEADFORMS_TAB_BUTTON = (By.XPATH, "//a[contains(@class, 'vkuiSimpleCell') and span/span[text()='Лид-формы и опросы']]")
+    NO_LEADFORMS_LABEL = (By.XPATH, "//h2[contains(@class, 'vkuiPlaceholder__header') and text()='Лид-форм пока нет']")
 
-    CREATE_LEAD_FORM_BUTTON= (By.XPATH, '//*[@id="leadads.leadforms"]/div/div[2]/div/div[2]/div/button')
+    CREATE_LEAD_FORM_BUTTON = (By.XPATH, "//button[contains(@class, 'LeadForms_createButton__BKYWP') and span/span[text()='Создать лид-форму']]")
+    NEW_LEAD_FORM_HEADER_LABEL = (By.XPATH, "//h2[contains(@class, 'ModalSidebarPage_title__Uu-FC') and text()='Новая лид-форма']")
 
-    NEW_LEAD_FORM_HEADER_LABEL= (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[1]/h2")
+    MORE_TEXT_BUTTON_INACTIVE = (By.XPATH, "//label[contains(@class, 'vkuiSegmentedControlOption')]//span[text()='Больше текста']")
+    SHORT_FORM_DESCRIPTION_HEADER_LABEL = (By.XPATH, "//h5[contains(@class, 'vkuiTypography') and contains(@class, 'FormItemRx_top__Onz0a') and text()='Описание формы']")
 
-    MORE_TEXT_BUTTON_INACTIVE= (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[4]/div/div/div/label[2]")
-    SHORT_FORM_DESCRIPTION_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[8]/div/h5")
+    LONG_FORM_DESCRIPTION_HEADER_LABEL = (By.XPATH, "//h5[contains(@class, 'vkuiTypography') and contains(@class, 'FormItemRx_top__Onz0a') and text()='Длинное описание (не более 2 переносов строк подряд)']")
+    LEAD_MAGNIT_BUTTON = (By.XPATH, "//label[contains(@class, 'vkuiSegmentedControlOption')]//span[text()='Лид-магнит']")
 
-    LONG_FORM_DESCRIPTION_HEADER_LABEL= (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[8]/h5")
-    LEAD_MAGNIT_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[4]/div/div/div/label[3]")
+    DISCOUNT_CHECKBOX = (By.XPATH, "//label[contains(@class, 'vkuiRadio')]//span[text()='Скидка']")
+    BONUS_CHECKBOX = (By.XPATH, "//label[contains(@class, 'vkuiRadio')]//span[text()='Бонус']")
 
-    DISCOUNT_CHECKBOX = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[8]/div/label[1]")
-    BONUS_CHECKBOX = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[8]/div/label[2]")
+    SIZE_OF_DISCOUNT_HEADER_LABEL = (By.XPATH, "//h5[contains(@class, 'vkuiTypography') and contains(@class, 'FormItemRx_top__Onz0a')]//span[text()='Размер скидки']")
+    BONUS_DESCRIPTION_HEADER_LABEL = (By.XPATH, "//h5[contains(@class, 'vkuiTypography') and contains(@class, 'FormItemRx_top__Onz0a')]//span[text()='Описание бонуса']")
 
-    SIZE_OF_DISCOUNT_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[9]/h5")
-    BONUS_DESCRIPTION_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[9]/h5")
+    UPLOAD_IMAGE_PLUS_BUTTON = (By.XPATH, "//div[contains(@class, 'vkuiImageBase') and contains(@class, 'vkuiAvatar')]")
+    MEDIA_LIBRARY_MENU_HEADER_LABEL = (By.XPATH, "//h2[contains(@class, 'vkuiTypography') and contains(@class, 'ModalSidebarPage_title__Uu-FC') and contains(@class, 'ModalSidebarPage_titleWithRight__Ywhiy') and text()='Медиатека']")
+    CLOSE_MEDIA_LIBRARY_MENU_BUTTON = (By.XPATH, "//button[contains(@class, 'vkuiIconButton') and contains(@class, 'vkuiIconButton--sizeY-none') and @aria-label='Close']")
+    CLICKABLE_IMAGE = (By.XPATH, "//div[contains(@class, 'ImageItem_imageItem__WiiIG') and contains(@class, 'ImageItem_active__1fy0f')]")
 
-    UPLOAD_IMAGE_PLUS_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[5]/div/div/div[1]/div")
-    MEDIA_LIBRARY_MENU_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[1]/h2")
-    CLOSE_MEDIA_LIBRARY_MENU_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div[2]/div[2]/div/div[1]/h2")
-    CLICKABLE_IMAGE = (By.XPATH, "//*[@id='media-library-image']/div/div")
+    COMPANY_NAME_INPUT = (By.XPATH, "//input[@placeholder='Название компании']")
+    TITLE_INPUT = HEADER_TEXT_INPUT = (By.XPATH, "//input[@placeholder='Текст заголовка']")
+    BONUS_DESCRIPTION_INPUT = (By.XPATH, "//input[@placeholder='Бонус']")
 
-    COMPANY_NAME_INPUT = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[6]/div/span[1]/input")
-    TITLE_INPUT = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[7]/div/span[1]/input")
-    BONUS_DESCRIPTION_INPUT = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[9]/span[1]/input")
+    FIRST_STEP_CONTINUE_BUTTON = (By.XPATH, "//button[contains(@class, 'vkuiButton') and contains(@class, 'vkuiButton--size-l') and contains(@class, 'vkuiButton--mode-primary') and @title='Продолжить']")
+    REQUIRED_FIELD_LABEL = (By.XPATH, "//span[contains(@class, 'vkuiTypography') and contains(@class, 'vkuiFormItem__bottom') and contains(@class, 'vkuiFootnote') and @role='alert']")
 
-    FIRST_STEP_CONTINUE_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[2]/div/button[2]")
-    REQUIRED_FIELD_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[9]/span[2]/div")
+    ADD_QUESTION_BUTTON = (By.XPATH, "//button[contains(@class, 'vkuiButton') and contains(@class, 'vkuiButton--size-l') and contains(@class, 'vkuiButton--mode-secondary') and contains(@class, 'vkuiButton--appearance-accent') and contains(@class, 'vkuiButton--align-center') and contains(@class, 'vkuiButton--stretched') and contains(@class, 'vkuiButton--with-icon') and contains(@class, 'vkuiTappable')]//span[contains(@class, 'vkuiButton__content') and text()='Добавить вопрос']")
+    QUESTION_NUMBER_ONE_ITEM_LABEL= (By.XPATH, "//div[contains(@class, 'Question_questionHeaderLeft__yHzJa') and text()='Вопрос № 1']")
 
-    ADD_QUESTION_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[1]/button")
-    QUESTION_NUMBER_ONE_ITEM_LABEL= (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[1]/div[2]/div/div[1]/div[1]")
+    QUESTION_TEXT_INPUT = (By.XPATH, "//span[contains(@class, 'vkuiFormField') and contains(@class, 'vkuiTextarea')]/textarea[@placeholder='Напишите вопрос']")
+    QUESTION_ANSWER_ONE = (By.XPATH, "//input[@class='vkuiTypography vkuiInput__el vkuiText vkuiText--sizeY-none' and @placeholder='Введите ответ']")
+    QUESTION_ANSWER_TWO = (By.CSS_SELECTOR, "input.vkuiTypography.vkuiInput__el.vkuiText.vkuiText--sizeY-none[placeholder='Введите ответ']")
 
-    QUESTION_TEXT_INPUT = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[1]/div[2]/div/div[2]/span[1]/textarea")
-    QUESTION_ANSWER_ONE = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[1]/div[2]/div/div[2]/div[1]/div/div[1]/div/span/input")
-    QUESTION_ANSWER_TWO = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[1]/div[2]/div/div[2]/div[1]/div/div[2]/div/span/input")
+    SECOND_STEP_CONTINUE_BUTTON = (By.XPATH, "//button[@type='submit' and @title='Продолжить' and contains(@class, 'vkuiButton') and contains(@class, 'vkuiButton--mode-primary')]")
+    RESULT_HEADER_LABEL = (By.XPATH, "//h4[contains(@class, 'vkuiTypography') and contains(@class, 'vkuiHeadline--level-1') and text()='Результат']")
 
-    SECOND_STEP_CONTINUE_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[2]/div/button[2]")
-    RESULT_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[1]/h4")
+    THIRD_STEP_CONTINUE_BUTTON = (By.XPATH, "//button[@title='Продолжить' and contains(@class, 'vkuiButton')]/span//span[text()='Продолжить']")
+    NOTIFICATIONS_HEADER_LABEL = (By.XPATH, "//h4[contains(@class, 'Settings_headline') and text()='Уведомления']")
 
-    THIRD_STEP_CONTINUE_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[2]/div/button[2]")
-    NOTIFICATIONS_HEADER_LABEL = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[1]/h4")
-
-    FIO_INPUT = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[6]/div[2]/span[1]/input")
-    ADDRESS_INPUT = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[1]/section/div/div[6]/div[3]/span[1]/input")
-    SAVE_BUTTON = (By.XPATH, "//*[@id='root']/div/div[2]/div/div[2]/form/div[2]/div[1]/div[2]/div/button[2]")
+    FIO_INPUT = (By.XPATH, "//input[@placeholder='Введите фамилию, имя и отчество']")
+    ADDRESS_INPUT = (By.XPATH, "//input[@placeholder='Введите адрес']")
+    SAVE_BUTTON = (By.XPATH, "//button[@title='Сохранить' and .//span[contains(text(), 'Сохранить')]]")
 
     ACTIVE_LEADFORD_ITEM = (By.XPATH, "/html/body/div[1]/div/div[1]/div[1]/div[1]/div/div[2]/div/main/div[2]/div/div/div[2]/section/div/div/div/div/div/div[2]/div[2]/div/div/div/div/div/div[1]/div[1]/div/div/div[4]")
