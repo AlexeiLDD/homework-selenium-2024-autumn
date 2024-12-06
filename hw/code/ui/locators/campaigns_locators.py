c38f5d1fcb1d3ff465b242ec698fa2fa2042159f
IlyaChgn <chugin.ilya2004@gmail.com>
2024-12-07 00:52:26 +0300

    Fixed campaign locators

--- hw/code/ui/locators/campaigns_locators.py
+++ hw/code/ui/locators/campaigns_locators.py
@@ -9,16 +9,16 @@ class CampaignsLocators:
 
 class CampaignSettingsLocators:
     CREATE_CAMPAIGN_BTN = (By.XPATH, '//button[@data-testid="create-button"]')
-    LEFT_SIDE_CAMPAIGN_TITLE = (By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[1]/div/div['
-                                          '1]/div/section/div/div/div[1]/div[1]/div[1]/div/div[2]/div/span')
-    CAMPAIGN_BUDGET = (By.XPATH, '//*[@id="root"]/div/div[1]/div[1]/div[1]/div/div[1]/div/section/div/div/div['
-                                 '2]/div/div/div/div/h4')
+    LEFT_SIDE_CAMPAIGN_TITLE = (By.CLASS_NAME, 'vkuiSimpleCell__middle')
+    CAMPAIGN_BUDGET = (By.XPATH, '//h4[@class="vkuiHeadline--level-1"')
+
+    # Блажайшим уникальным элементом является модальное окно
     DIALOG_CLOSE_BTN = (By.XPATH, '//*[@id="_modal_28"]/div/div/div[2]/div[1]/div/div/div/div/button')
 
-    CAMPAIGN_TITLE = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div/section/div/div/div[1]/div')
+    CAMPAIGN_TITLE = (By.XPATH, '//div[contains(@class, "EditableTitle_container")]')
     TITLE_INPUT = (By.TAG_NAME, 'textarea')
 
-    CONTINUE_BTN = (By.XPATH, '//*[@id="footer"]/div/div/div[2]/div[2]/button')
+    CONTINUE_BTN = (By.XPATH, '//div[contains(@class, "CreateFooter_actionsWrap")]/button[1]')
 
 
 class CampaignActionsLocators:
@@ -30,21 +30,18 @@ class CampaignActionsLocators:
     CATALOG_ACTION_BTN = (By.XPATH, '//div[@data-id="ecomm"]')
     LEADADS_ACTION_BTN = (By.XPATH, '//div[@data-id="leadads"]')
 
-    FEATURES_INPUT_SITE_ACTION = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div/section[2]/form/div['
-                                            '3]/div/span/textarea')
+    FEATURES_INPUT_SITE_ACTION = (By.XPATH, '//textarea[@placeholder="Опишите ваше предложение"]')
     ACTION_INPUT_SITE_ACTION = (By.XPATH, '//input[@role="combobox"]')
     OPTIMIZATION_SWITCH_SITE_ACTION = (By.XPATH, '//input[@data-testid="budget-optimization"]')
     DATES_INPUTS_SITE_ACTION = (By.XPATH, '//*[@data-name="dates"]')
 
-    OBJECT_RADIOBTN_CATALOG_ACTION = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div/section[2]/form/div['
-                                                '1]/div')
+    OBJECT_RADIOBTN_CATALOG_ACTION = (By.XPATH, '//form/div[1]')
     DOMAIN_INPUT_CATALOG_ACTION = (By.XPATH, '//input[@placeholder="Введите ссылку на сайт"]')
     GOODS_INPUT_CATALOG_ACTION = (By.XPATH, '//input[@role="combobox"]')
 
-    POLL_RADIOBTN = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div/section[2]/form/div[1]/div/label[2]/div')
+    POLL_RADIOBTN = (By.XPATH, '//*[@class="vkuiInternalRadioGroup"]/label[2]')
     LEADFORM_SELECT_LEADADS_ACTION = (By.XPATH, '//input[@data-testid="lead-form-select"]')
-    SPLIT_TEST_SWITCH_LEADADS_ACTION = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div/section[2]/form/div['
-                                                  '2]/div/div[2]/label')
+    SPLIT_TEST_SWITCH_LEADADS_ACTION = (By.XPATH, '//*[contains(@class, "LeadFormObject_abFormItem")]/label/div[1]')
     TYPE_COMBOBOX_LEADADAS_ACTION = (By.XPATH, '//input[@role="combobox"]')
 
 
@@ -61,24 +58,23 @@ class CampaignBrandingLocators:
 
 
 class AdvertsGroupsLocators:
-    REGION_BTN = (By.XPATH, '/html/body/div[1]/div/div[1]/div[1]/div[1]/div/div[2]/div/main/div['
-                            '2]/div/div/div/div/div/div[1]/section[2]/div[2]/fieldset/div/div/div[1]/div[2]/button[2]')
-    AUDIENCE_VALUE = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[2]/section/div/div/div/div/div/div['
-                                '3]/div[2]/div[2]')
-    CONTINUE_BTN = (By.XPATH, '//*[@id="footer"]/div/div/div[2]/div[2]/button')
+    REGION_BTN = (By.XPATH, '//div[contains(@class, "RegionsQuickSelection_wrapper")]/button[2]')
+    AUDIENCE_VALUE = (By.XPATH, '//span[contains(@class, "Prediction_itemAuditoryValue")]')
+    CONTINUE_BTN = (By.XPATH, '//div[contains(@class, "CreateFooter_actionsWrap")]/button')
 
-    NAME_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/section')
-    BETS_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/section[1]')
-    REGIONS_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/section[2]')
-    AUDIENCE_EXPANDING_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/section[3]')
-    DEMOGRAPHIC_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/div/section[1]')
-    BEHAVIOR_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/div/section[2]')
-    AUDIENCE_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/div/section[3]')
-    DEVICES_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/div/section[4]')
-    URL_PARAMS_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/div/section[5]')
-    PLACES_CONTAINER = (By.XPATH, '//*[@id="new_ad_create"]/div/div/div/div/div[1]/div/section[6]')
+    # Для всех нижеперечисленных локаторов ближайший элемент с уникальным XPATH - контейнер GroupForm
+    NAME_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/section')
+    BETS_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/section[1]')
+    REGIONS_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/section[2]')
+    AUDIENCE_EXPANDING_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/section[3]')
+    DEMOGRAPHIC_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/div/section[1]')
+    BEHAVIOR_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/div/section[2]')
+    AUDIENCE_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/div/section[3]')
+    DEVICES_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/div/section[4]')
+    URL_PARAMS_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/div/section[5]')
+    PLACES_CONTAINER = (By.XPATH, '//*[contains(@class, "GroupForm_container")]/div[1]/div/section[6]')
 
 
 class AdvertsLocators:
     ADVERT_TITLE = (By.TAG_NAME, 'h2')
-    PUBLISH_BTN = (By.XPATH, '//*[@id="footer"]/div/div/div[2]/div[2]/button/span')
+    PUBLISH_BTN = (By.XPATH, '//div[contains(@class, "CreateFooter_actionsWrap")]/button')
